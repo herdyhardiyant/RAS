e283d6415f0f70836c72c3a3aa3373c48cdfa0ca
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2022-12-02 09:08:51 +0700

    Player knockback

--- Assets/Environment/Scripts/BarrelRoll.cs
+++ Assets/Environment/Scripts/BarrelRoll.cs
@@ -7,17 +7,16 @@ namespace Environment.Scripts
     public class BarrelRoll : MonoBehaviour
     {
         //TODO Barrel Roll forward, explode on impact
-        
+
         [SerializeField] private string barrelName;
         [SerializeField] private float maxAngularVelocity = 20f;
         [SerializeField] private float barrelSpeed = 100f;
         public string Name => barrelName;
 
         private Rigidbody _rigidbody;
-        
         private void Awake()
         {
-            _rigidbody  = GetComponent<Rigidbody>();
+            _rigidbody = GetComponent<Rigidbody>();
             _rigidbody.maxAngularVelocity = maxAngularVelocity;
         }
 
@@ -26,11 +25,15 @@ namespace Environment.Scripts
             transform.localRotation = Quaternion.Euler(0, 0, 90);
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
-            _rigidbody.angularVelocity = -Vector3.forward * barrelSpeed;
-            if(transform.position.y < -10)
+            _rigidbody.AddTorque(-Vector3.forward * barrelSpeed);
+
+            if (transform.position.y < -10)
+            {
                 ObjectPool.SharedInstance.ReturnObjectToPool(gameObject);
+            }
         }
+        
     }
 }
--- Assets/Environment/Scripts/BarrelRollSpawner.cs
+++ Assets/Environment/Scripts/BarrelRollSpawner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Environment.Scripts
@@ -11,34 +12,38 @@ namespace Environment.Scripts
         // Spawn position from inside the container
         // Barrel rotation 90 degrees in the z axis
         // Barrel roll and move forward
-        
+
         // If the player hit the barrel
         // the barrel will return to pool
         // the player will get knocked back
-        
+
         [SerializeField] private GameObject barrelPrefabs;
         [SerializeField] private Transform spawnPoint;
-        [SerializeField] private float spawnDelay = 1f;
+        [SerializeField] private float spawnDelay = 3f;
 
         private GameObject _barrel;
         private Rigidbody _barrelRb;
-        
+
         private void Start()
-        { 
-            _barrel = ObjectPool.SharedInstance.GetPooledObject(barrelPrefabs.name);
-          
-            _barrel.transform.position = spawnPoint.position;
-            _barrel.transform.forward = spawnPoint.forward;
-            _barrel.transform.parent = spawnPoint;
-
-            // Set delay to spawn barrel
-            spawnDelay = Time.time + 1f;
-            
-        }
-
-        private void Update()
         {
-           
+            StartCoroutine(SpawnBarrel());
+        }
+        
+        private IEnumerator SpawnBarrel()
+        {
+            while (true)
+            {
+                
+                yield return new WaitForSeconds(spawnDelay);
+                
+                _barrel = ObjectPool.SharedInstance.GetPooledObject(barrelPrefabs.name);
+                _barrel.transform.position = spawnPoint.position;
+                _barrel.transform.forward = spawnPoint.forward;
+                _barrel.transform.parent = spawnPoint;
+                _barrel.transform.localRotation = Quaternion.Euler(0, 0, 90);
+            }
+            
+
         }
     }
 }
