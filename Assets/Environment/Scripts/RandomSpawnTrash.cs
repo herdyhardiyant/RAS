50460070b73a02cb9fded02cdd362f128dfef921
ahmedamedfz <ahmadfariz11@gmail.com>
2022-12-03 05:03:24 +0700

    updatekecil

--- Assets/Environment/Scripts/RandomSpawnTrash.cs
+++ Assets/Environment/Scripts/RandomSpawnTrash.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+namespace Environment.Scripts{
     public class RandomSpawnTrash : MonoBehaviour
     {
         public GameObject[] tipeSampah;
@@ -23,19 +23,23 @@ using UnityEngine;
            float posisiZacak = Random.Range(-0.5f,0.5f);
            Vector3 tmPos = transform.position;
            if (tmPos.x == 0)
-            tmPos.x = Random.Range(-45f,60f);
+            tmPos.x = Random.Range(-25f,35f);
            if (tmPos.z == 0)
-            tmPos.z = Random.Range(-27f,27f);
-           tmPos.x = tmPos.x + tmPos.x*posisiXacak;
-           tmPos.z = tmPos.z + tmPos.z*posisiZacak;
-           tmPos.y = posisiSpawn;
-           transform.position=tmPos;
-           posisiRandom = transform.position;
-           int randomSampah = Random.Range(0,tipeSampah.Length);
-            Instantiate(tipeSampah[randomSampah],posisiRandom,Quaternion.identity);
+            tmPos.z = Random.Range(-20f,20f);
+            tmPos.x = tmPos.x + tmPos.x*posisiXacak;
+            tmPos.z = tmPos.z + tmPos.z*posisiZacak;
+            tmPos.y = posisiSpawn;
+            transform.position=tmPos;
+            posisiRandom = transform.position;
+            int randomSampah = Random.Range(0,tipeSampah.Length);
+            var sampah = ObjectPool.Instance.GetPooledObject(tipeSampah[randomSampah].name);
+            // Instantiate(tipeSampah[randomSampah],posisiRandom,Quaternion.identity);
+           sampah.transform.position = posisiRandom;
         }
         jalanfungsi = false;
         }
+    
+        }
+        
     }
-    }
-
+}
