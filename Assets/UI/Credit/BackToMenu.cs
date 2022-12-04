a864f740d5ca430141331a4d1cafc612959e074f
oYUDHAo <code.yudha@gmail.com>
2022-12-04 16:45:16 +0700

    Credit sceneAnimation

--- Assets/UI/Credit/BackToMenu.cs
+++ Assets/UI/Credit/BackToMenu.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace RAS
 {
     public class BackToMenu : MonoBehaviour
     {
+        // [SerializeField] private Animator creditScene;
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape)){
                 UnloadAddictive("Credit");
             }
+
+            // if (creditScene == )
+            // {
+            //     UnloadAddictive("Credit");
+            // }
         }
         public static void UnloadAddictive(string sceneName){
             SceneLoader.UnloadAddictive(sceneName);
         }
+
+        
     }
 }
